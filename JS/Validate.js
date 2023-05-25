0b4555c3944cddf6b8ba4552df5ee949449b691d
JMalpica1 <131896878+JMalpica1@users.noreply.github.com>
2023-05-25 17:40:39 -0400

    Validadores
    Se corrigieron los Validadores

--- JS/Validate.js
+++ JS/Validate.js
@@ -1,54 +1,176 @@
-function validarFormulario() {
-  var rut = document.getElementById("rut").value;
-  var calle = document.getElementById("calle").value;
-  var ciudad = document.getElementById("ciudad").value;
-  var nombre = document.getElementById("nombre").value;
-  var email = document.getElementById("email").value;
-  var contraseña = document.getElementById("contraseña").value;
-  var RepetirContraseña = document.getElementById("RepetirContraseña").value;
+$(function () {
+  $("form[name='registration']").validate({
+    rules: {
+      firstname: "required",
 
-  var expresionRegular = /^([a-zA-Z0-9]+-)*[a-zA-Z0-9]+$/;
-  if(!expresionRegular.test(rut)){
+      lastname: "required",
 
-alert("Por favor, ingresa un rut válido");
+      direccion: "required",
 
-  }
+      ciudad: "required",
 
-  
-  if (calle===""){
-    alert("Ingrese una dirección");
-    return false;
-  }
+      codigoZip: "required",
 
-  if(ciudad===""){
-    alert("Ingresa una ciudad");
-    return false;
-  }
-  
+      email: {
+        required: true,
 
-  if (nombre === "") {
-    alert("Por favor, ingresa tu nombre");
-    return false;
-  }
+        email: true,
+      },
+    },
 
+    messages: {
+      firstname: "Por favor, introduzca su nombre",
 
-  var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (!emailRegex.test(email)) {
-    alert("Por favor, ingresa un correo electrónico válido");
-    return false;
-  }
-  
-  var expresionRegular = /^.{8,}$/;
+      lastname: "Por favor, introduzca su apellido",
 
-  if (!expresionRegular.test(contraseña)) {
-    return false;
-  }
-  
-  if (contraseña !== RepetirContraseña) {
-    return false;
-  }
+      email: "Por favor, introduce una dirección de correo electrónico válida",
 
-  
-  alert("Formulario válido");
-  return true;
-}
+      direccion: "Por favor, ingrese una dirección",
+
+      ciudad: "Por favor, ingrese una ciudad",
+
+      codigoZip: "Por favor, ingrese un Código Zip",
+    },
+
+    submitHandler: function (form) {
+      form.submit();
+    },
+  });
+});
+
+$(document).ready(function () {
+  $.validator.addMethod(
+    "rutChileno",
+    function (value, element) {
+      value = value.replace(/[.-]/g, "");
+
+      if (value.length < 8 || value.length > 9) {
+        return false;
+      }
+
+      var validChars = "0123456789K";
+      var lastChar = value.charAt(value.length - 1).toUpperCase();
+      if (validChars.indexOf(lastChar) == -1) {
+        return false;
+      }
+
+      var rut = parseInt(value.slice(0, -1), 10);
+      var factor = 2;
+      var sum = 0;
+      var digit;
+      while (rut > 0) {
+        digit = rut % 10;
+        sum += digit * factor;
+        rut = Math.floor(rut / 10);
+        factor = factor === 7 ? 2 : factor + 1;
+      }
+      var dv = 11 - (sum % 11);
+      dv = dv === 11 ? "0" : dv === 10 ? "K" : dv.toString();
+
+      return dv === lastChar;
+    },
+    "Por favor ingrese un RUT válido."
+  );
+
+  $("#formulario1").validate({
+    rules: {
+      nombre: "required",
+
+      apellido: "required",
+
+      direccion: "required",
+
+      ciudad: "required",
+
+      codigoZip: "required",
+
+      codigo: "required",
+
+      id: "required",
+
+      descripcion : "required",
+
+      precio : "required",
+
+      descuentosus: "required",
+
+      descuentof:"required",
+
+      rut: {
+        required: true,
+        rutChileno: true,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      password: {
+        required: true,
+        minlength: 5,
+      },
+      password2: {
+        required: true,
+        equalTo: "#password",
+      },
+    },
+    messages: {
+      nombre: {
+        required: "El nombre es un campo obligatorio",
+      },
+
+      apellido: {
+        required: "El apellido es un campo obligatorio",
+      },
+
+      direccion: {
+        required: "La dirección es un campo obligatorio",
+      },
+
+      ciudad: {
+        required: "La ciudad es un campo requerido",
+      },
+
+      codigoZip: {
+        required: "Debe ingresar un código zip",
+      },
+
+      rut: {
+        required: "El rut es un campo obligatorio",
+        rutChileno: "El formato del rut no es válido",
+      },
+      email: {
+        required: "El email es un campo requerido",
+        email: "El email no cumple el formato de un correo",
+      },
+      password: {
+        required: "La contraseña es una campo obligatorio",
+        minlength: "Mínimo 5 caracteres",
+      },
+      password2: {
+        required: "Repita la contraseña anterior",
+        equalTo: "Debe ser igual al campo contraseña",
+      },
+
+      codigo:{
+        required: "debe ingresar un codigo"
+      },
+
+      id:{
+        required: "debe ingresar un id"
+      },
+
+      precio:{
+
+        required: "debe ingresar un precio"
+      },
+
+      descuentosus:{
+        required : "Debe ingresar descuento"
+      },
+
+      descuentof:{
+        required :"Debe ingresar descuento"
+      }
+    },
+  });
+});
